e08b317fb3bcb858afd7131f19fd1935f08b462c
Doug Kerwin <dwkerwin@gmail.com>
2018-08-16 10:41:37 -0400

    added s3 bucket for userdata

--- terraform/dev/dev-us-east-1/s3.tf
+++ terraform/dev/dev-us-east-1/s3.tf
@@ -1,5 +1,18 @@
-resource "aws_s3_bucket" "terraform_state" {
+resource "aws_s3_bucket" "deploy" {
   bucket = "forgotpw-deploy-${var.environment}"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket" "userdata" {
+  bucket = "forgotpw-userdata-${var.environment}"
   acl    = "private"
 
   versioning {
