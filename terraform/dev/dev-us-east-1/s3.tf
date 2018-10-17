2de99dd3ac6a15d68370c6efd2b0069a89bc3dfb
Doug Kerwin <dwkerwin@gmail.com>
2018-10-16 20:52:07 -0400

    added usertokens s3 bucket

--- terraform/dev/dev-us-east-1/s3.tf
+++ terraform/dev/dev-us-east-1/s3.tf
@@ -66,3 +66,59 @@ resource "aws_s3_bucket_policy" "userdata" {
 }
 POLICY
 }
+
+resource "aws_s3_bucket" "usertokens" {
+  bucket = "forgotpw-usertokens-${var.environment}"
+  acl    = "private"
+
+  versioning {
+    enabled = false
+  }
+
+  server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm     = "AES256"
+        }
+      }
+    }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "usertokens" {
+  bucket = "${aws_s3_bucket.usertokens.id}"
+  policy =<<POLICY
+{
+    "Version": "2012-10-17",
+    "Id": "PutObjPolicy",
+    "Statement": [
+          {
+              "Sid": "DenyIncorrectEncryptionHeader",
+              "Effect": "Deny",
+              "Principal": "*",
+              "Action": "s3:PutObject",
+              "Resource": "arn:aws:s3:::${aws_s3_bucket.usertokens.bucket}/*",
+              "Condition": {
+                      "StringNotEquals": {
+                              "s3:x-amz-server-side-encryption": "AES256"
+                        }
+              }
+          },
+          {
+              "Sid": "DenyUnEncryptedObjectUploads",
+              "Effect": "Deny",
+              "Principal": "*",
+              "Action": "s3:PutObject",
+              "Resource": "arn:aws:s3:::${aws_s3_bucket.usertokens.bucket}/*",
+              "Condition": {
+                      "Null": {
+                              "s3:x-amz-server-side-encryption": true
+                      }
+              }
+          }
+    ]
+}
+POLICY
+}
